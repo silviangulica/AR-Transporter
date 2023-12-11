454ebdd53c9ee69c3caf49b72e1c00e427f44046
Silvian Gulica <gulica.sv@gmail.com>
2023-12-11 23:22:53 +0200

    Update

---
+++ Assets/Scripts/Services/MainMenu.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
